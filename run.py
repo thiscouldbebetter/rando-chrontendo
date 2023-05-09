c334290a7c5755dec5170248c868d5204fe4f00e
Geoff Miller <geoffrey.s.miller@gmail.com>
2023-05-08 22:19:17 -0700

    alt text with episode/timestamp; black/near-black screen filtering; clean-up/reorganization

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import logging
+import math
 import os
 import time
 from random import randint
 
 import cv2
 import pytumblr
-import twitter
 from cohost.models.block import AttachmentBlock as CohostAttachmentBlock
 from cohost.models.user import User as CohostUser
 from mastodon import Mastodon
-
+from twitter import *
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,87 +20,80 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-videos_directory = os.environ.get("VIDEOS_DIR")
+VIDEOS_DIRECTORY = os.environ.get("VIDEOS_DIR")
 
-twitter_consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
-twitter_consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
-twitter_access_token_key = os.environ.get("TWITTER_ACCESS_TOKEN_KEY")
-twitter_access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
-
-cohost_email = os.environ.get("COHOST_EMAIL")
-cohost_password = os.environ.get("COHOST_PASSWORD")
-
-mastodon_access_token = os.environ.get("MASTODON_ACCESS_TOKEN")
-mastodon_api_base_url = os.environ.get("MASTODON_API_BASE_URL")
-
-tumblr_consumer_key = os.environ.get("TUMBLR_CONSUMER_KEY")
-tumblr_consumer_secret = os.environ.get("TUMBLR_CONSUMER_SECRET")
-tumblr_oauth_token = os.environ.get("TUMBLR_OAUTH_TOKEN")
-tumblr_oauth_secret = os.environ.get("TUMBLR_OAUTH_SECRET")
+TWITTER_CREDENTIALS = {
+    "token": os.environ.get("TWITTER_ACCESS_TOKEN_KEY"),
+    "token_secret": os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"),
+    "consumer_key": os.environ.get("TWITTER_CONSUMER_KEY"),
+    "consumer_secret": os.environ.get("TWITTER_CONSUMER_SECRET"),
+}
+COHOST_CREDENTIALS = {
+    "email": os.environ.get("COHOST_EMAIL"),
+    "password": os.environ.get("COHOST_PASSWORD"),
+}
+MASTODON_CREDENTIALS = {
+    "access_token": os.environ.get("MASTODON_ACCESS_TOKEN"),
+    "api_base_url": os.environ.get("MASTODON_API_BASE_URL"),
+}
+TUMBLR_CREDENTIALS = {
+    "consumer_key": os.environ.get("TUMBLR_CONSUMER_KEY"),
+    "consumer_secret": os.environ.get("TUMBLR_CONSUMER_SECRET"),
+    "oauth_token": os.environ.get("TUMBLR_OAUTH_TOKEN"),
+    "oauth_secret": os.environ.get("TUMBLR_OAUTH_SECRET"),
+}
 
 
 def run():
-    video_files = os.listdir(videos_directory)
-    file_to_grab = randint(0, len(video_files) - 1)
-    file_name = video_files[file_to_grab]
-
-    vid = cv2.VideoCapture("{}/{}".format(videos_directory, file_name))
-
-    total_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
-    frame_to_grab = randint(200, total_frames - 200)
-    vid.set(cv2.CAP_PROP_POS_FRAMES, frame_to_grab)
-
-    ret, curframe = vid.read()
-    cv2.imwrite("image.jpg", curframe)
-    vid.release()
-    cv2.destroyAllWindows()
+    post = RandoChrontendoPost()
 
     try:
-        post_twitter()
+        post_twitter(post)
     except Exception as e:
         logger.error(f"Twitter post failed: {e}")
 
     try:
-        post_cohost()
+        post_cohost(post)
     except Exception as e:
         logger.error(f"Cohost post failed: {e}")
 
     try:
-        post_tumblr()
+        post_tumblr(post)
     except Exception as e:
         logger.error(f"Tumblr post failed: {e}")
 
     try:
-        post_mastodon()
+        post_mastodon(post)
     except Exception as e:
         logger.error(f"Mastodon post failed: {e}")
 
 
-def post_twitter():
-    api = twitter.Api(
-        consumer_key=twitter_consumer_key,
-        consumer_secret=twitter_consumer_secret,
-        access_token_key=twitter_access_token_key,
-        access_token_secret=twitter_access_token_secret,
-        sleep_on_rate_limit=True,
+def post_twitter(post):
+    twitter_upload = Twitter(
+        domain="upload.twitter.com", auth=OAuth(**TWITTER_CREDENTIALS)
     )
+    with open("image.jpg", "rb") as image_data:
+        media_id = twitter_upload.media.upload(media=image_data.read())[
+            "media_id_string"
+        ]
+    twitter_upload.media.metadata.create(
+        _json={"media_id": media_id, "alt_text": {"text": post.get_alt_text()}}
+    )
+    twitter = Twitter(auth=OAuth(**TWITTER_CREDENTIALS))
+    twitter.statuses.update(media_ids=media_id)
 
-    api.PostUpdate(status="", media="image.jpg")
 
-
-def post_cohost():
-    user = CohostUser.login(cohost_email, cohost_password)
+def post_cohost(post):
+    user = CohostUser.login(**COHOST_CREDENTIALS)
     project = user.getProject("randochrontendo")
 
-    blocks = [CohostAttachmentBlock("image.jpg")]
+    blocks = [CohostAttachmentBlock("image.jpg", alt_text=post.get_alt_text())]
     project.post("", blocks)
 
 
-def post_mastodon():
-    mastodon = Mastodon(
-        access_token=mastodon_access_token, api_base_url=mastodon_api_base_url
-    )
-    media = mastodon.media_post("image.jpg")
+def post_mastodon(post):
+    mastodon = Mastodon(**MASTODON_CREDENTIALS)
+    media = mastodon.media_post("image.jpg", description=post.get_alt_text())
     timeout = 1
     while media["url"] is None:
         time.sleep(timeout)
@@ -109,14 +102,50 @@ def post_mastodon():
     mastodon.status_post("", media_ids=[media["id"]])
 
 
-def post_tumblr():
-    client = pytumblr.TumblrRestClient(
-        tumblr_consumer_key,
-        tumblr_consumer_secret,
-        tumblr_oauth_token,
-        tumblr_oauth_secret,
+def post_tumblr(post):
+    client = pytumblr.TumblrRestClient(**TUMBLR_CREDENTIALS)
+    client.create_photo(
+        "randochrontendo.tumblr.com", data="image.jpg", caption=post.get_alt_text()
     )
-    client.create_photo("randochrontendo.tumblr.com", data="image.jpg")
+
+
+class RandoChrontendoPost:
+    def __init__(self):
+        self._get_video_file()
+        self._write_image()
+
+    def get_alt_text(self):
+        return f"{self.video_name} ({self.timestamp})"
+
+    def _get_video_file(self):
+        video_files = os.listdir(VIDEOS_DIRECTORY)
+        file_to_grab = randint(0, len(video_files) - 1)
+        self.file_name = video_files[file_to_grab]
+        self.video_name = (self.file_name.split(".")[0]).strip()
+
+    def _write_image(self):
+        video = cv2.VideoCapture("{}/{}".format(VIDEOS_DIRECTORY, self.file_name))
+        total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+
+        count_non_zero = 0
+        while count_non_zero <= 2500:
+            frame_to_grab = randint(1, total_frames)
+            video.set(cv2.CAP_PROP_POS_FRAMES, frame_to_grab)
+            _, curframe = video.read()
+            cv2.imwrite("image.jpg", curframe)
+            img = cv2.imread("image.jpg", 0)
+            count_non_zero = cv2.countNonZero(img)
+
+        self._get_timestamp(video.get(cv2.CAP_PROP_POS_MSEC))
+
+        video.release()
+        cv2.destroyAllWindows()
+
+    def _get_timestamp(self, milliseconds):
+        seconds = math.floor((milliseconds / 1000) % 60)
+        minutes = math.floor((milliseconds / (1000 * 60)) % 60)
+        hours = math.floor((milliseconds / (1000 * 60 * 60)) % 24)
+        self.timestamp = f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 if __name__ == "__main__":
