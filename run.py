5fe14c0092f45b113c57cc6f0346294e4789b39a
Geoff Miller <geoffrey.s.miller@gmail.com>
2023-05-09 14:28:38 -0700

    some more cleanup

--- run.py
+++ run.py
@@ -32,6 +32,7 @@ COHOST_CREDENTIALS = {
     "email": os.environ.get("COHOST_EMAIL"),
     "password": os.environ.get("COHOST_PASSWORD"),
 }
+COHOST_PROJECT = os.environ.get("COHOST_PROJECT")
 MASTODON_CREDENTIALS = {
     "access_token": os.environ.get("MASTODON_ACCESS_TOKEN"),
     "api_base_url": os.environ.get("MASTODON_API_BASE_URL"),
@@ -42,80 +43,80 @@ TUMBLR_CREDENTIALS = {
     "oauth_token": os.environ.get("TUMBLR_OAUTH_TOKEN"),
     "oauth_secret": os.environ.get("TUMBLR_OAUTH_SECRET"),
 }
+TUMBLR_BLOG = os.environ.get("TUMBLR_BLOG")
 
 
 def run():
     post = RandoChrontendoPost()
 
     try:
-        post_twitter(post)
+        post.post_twitter()
     except Exception as e:
         logger.error(f"Twitter post failed: {e}")
 
     try:
-        post_cohost(post)
+        post.post_cohost()
     except Exception as e:
         logger.error(f"Cohost post failed: {e}")
 
     try:
-        post_tumblr(post)
+        post.post_tumblr()
     except Exception as e:
         logger.error(f"Tumblr post failed: {e}")
 
     try:
-        post_mastodon(post)
+        post.post_mastodon()
     except Exception as e:
         logger.error(f"Mastodon post failed: {e}")
 
 
-def post_twitter(post):
-    twitter_upload = Twitter(
-        domain="upload.twitter.com", auth=OAuth(**TWITTER_CREDENTIALS)
-    )
-    with open("image.jpg", "rb") as image_data:
-        media_id = twitter_upload.media.upload(media=image_data.read())[
-            "media_id_string"
-        ]
-    twitter_upload.media.metadata.create(
-        _json={"media_id": media_id, "alt_text": {"text": post.get_alt_text()}}
-    )
-    twitter = Twitter(auth=OAuth(**TWITTER_CREDENTIALS))
-    twitter.statuses.update(media_ids=media_id)
-
-
-def post_cohost(post):
-    user = CohostUser.login(**COHOST_CREDENTIALS)
-    project = user.getProject("randochrontendo")
-
-    blocks = [CohostAttachmentBlock("image.jpg", alt_text=post.get_alt_text())]
-    project.post("", blocks)
-
-
-def post_mastodon(post):
-    mastodon = Mastodon(**MASTODON_CREDENTIALS)
-    media = mastodon.media_post("image.jpg", description=post.get_alt_text())
-    timeout = 1
-    while media["url"] is None:
-        time.sleep(timeout)
-        media = mastodon.media(media)
-        timeout *= 2
-    mastodon.status_post("", media_ids=[media["id"]])
-
-
-def post_tumblr(post):
-    client = pytumblr.TumblrRestClient(**TUMBLR_CREDENTIALS)
-    client.create_photo(
-        "randochrontendo.tumblr.com", data="image.jpg", caption=post.get_alt_text()
-    )
-
-
 class RandoChrontendoPost:
-    def __init__(self):
+    def __init__(self, image_file_name="image.jpg"):
+        self.image_file_name = image_file_name
         self._get_video_file()
         self._write_image()
 
-    def get_alt_text(self):
+    @property
+    def alt_text(self):
         return f"{self.video_name} ({self.timestamp})"
+
+    def post_twitter(self):
+        twitter_upload = Twitter(
+            domain="upload.twitter.com", auth=OAuth(**TWITTER_CREDENTIALS)
+        )
+        with open(self.image_file_name, "rb") as image_data:
+            media_id = twitter_upload.media.upload(media=image_data.read())[
+                "media_id_string"
+            ]
+        twitter_upload.media.metadata.create(
+            _json={"media_id": media_id, "alt_text": {"text": self.alt_text}}
+        )
+        twitter = Twitter(auth=OAuth(**TWITTER_CREDENTIALS))
+        twitter.statuses.update(media_ids=media_id)
+
+    def post_cohost(self):
+        user = CohostUser.login(**COHOST_CREDENTIALS)
+        project = user.getProject(COHOST_PROJECT)
+        blocks = [CohostAttachmentBlock(self.image_file_name, alt_text=self.alt_text)]
+        project.post("", blocks)
+
+    def post_tumblr(self):
+        client = pytumblr.TumblrRestClient(**TUMBLR_CREDENTIALS)
+        client.create_photo(
+            TUMBLR_BLOG,
+            data=self.image_file_name,
+            caption=self.alt_text,
+        )
+
+    def post_mastodon(self):
+        mastodon = Mastodon(**MASTODON_CREDENTIALS)
+        media = mastodon.media_post(self.image_file_name, description=self.alt_text)
+        timeout = 1
+        while media["url"] is None:
+            time.sleep(timeout)
+            media = mastodon.media(media)
+            timeout *= 2
+        mastodon.status_post("", media_ids=[media["id"]])
 
     def _get_video_file(self):
         video_files = os.listdir(VIDEOS_DIRECTORY)
@@ -132,16 +133,16 @@ class RandoChrontendoPost:
             frame_to_grab = randint(1, total_frames)
             video.set(cv2.CAP_PROP_POS_FRAMES, frame_to_grab)
             _, curframe = video.read()
-            cv2.imwrite("image.jpg", curframe)
-            img = cv2.imread("image.jpg", 0)
+            cv2.imwrite(self.image_file_name, curframe)
+            img = cv2.imread(self.image_file_name, 0)
             count_non_zero = cv2.countNonZero(img)
 
-        self._get_timestamp(video.get(cv2.CAP_PROP_POS_MSEC))
+        self._set_timestamp(video.get(cv2.CAP_PROP_POS_MSEC))
 
         video.release()
         cv2.destroyAllWindows()
 
-    def _get_timestamp(self, milliseconds):
+    def _set_timestamp(self, milliseconds):
         seconds = math.floor((milliseconds / 1000) % 60)
         minutes = math.floor((milliseconds / (1000 * 60)) % 60)
         hours = math.floor((milliseconds / (1000 * 60 * 60)) % 24)
